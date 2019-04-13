176df0d9b13d8bc7de08ed2e6b436db187951f87
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-04-13 09:54:08 -0500

    Created initial code for server.

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 //Sets up ability to parse the data
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
+var reservations = [];
+var tables = [];
+var waitlist = [];
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
