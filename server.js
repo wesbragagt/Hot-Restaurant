da6ce7be63f6a5ae779d4df8bd15508d595f1ef0
Cody <cody.tracy92@gmail.com>
2019-04-13 10:15:48 -0500

    added in functions to fill the tables/waitlist arrays

--- server.js
+++ server.js
@@ -16,10 +16,35 @@ var reservations = [];
 var tables = [];
 var waitlist = [];
 
+//Create tables info
+function currentTables(){
+    for(var i = 0; i<4; i++){
+        tables.push(reservations[i]);
+    }
+};
+currentTables();
+
+//Create waitlist info
+function currentWait(){
+    for(var i = 5; i<reservations.length; i++){
+        waitlist.push(reservations[i]);
+    }
+};
+currentWait();
+
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 
+app.get("/reservation", function (req, res) {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+
+
+
+
+//Listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
